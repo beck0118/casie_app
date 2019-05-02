405a5e9850916e64f0c8beb72d19a5d2e975ce50
beck0118 <k2jxxx@gmail.com>
2019-05-03 00:35:22 +0900

    ビュ＾

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-1  
+  get "/" => "home#top"
+  get "/show" => "users#show"
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
