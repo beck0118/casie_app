756f163e06d3efff40694165f60be29510836649
beck0118 <k2jxxx@gmail.com>
2019-05-03 18:15:07 +0900

    ddd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
-  get "/show" => "users#show"
-  resources :users, only: [:show]
+get "/" => "home#top"
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
